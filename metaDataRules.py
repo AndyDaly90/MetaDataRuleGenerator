5ee654c6a0f2a513ff77a29f744b2b3a0f781faf
Andrew Daly <andrew.daly@students.ittralee.ie>
2017-02-21 16:02:58 +0000

    First, Commit Rules added for string and date processing.

--- metaDataRules.py
+++ metaDataRules.py
@@ -30,6 +30,43 @@ class MetaDataRules:
         rule = "NumberStringToDate[[-SEP-]]" \
                + str(destination_field_id) + "[[-SEP-]]" \
                + str(source_field_id) + "[[-SEP-]]True[[-SEP-]][" \
-               "[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[" \
-               "-SEP-]][[-SEP-]][[-SEP-]]"
+                                        "[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[" \
+                                        "-SEP-]][[-SEP-]][[" \
+                                        "-SEP-]][[-SEP-]][[-SEP-]]"
+        return rule
+
+    @staticmethod
+    def crossDbInsert(source_id, source_form, source_field_values,
+                      destination_id, destination_field_values):
+        source_str = ""
+        dest_str = ""
+
+        for i in range(len(source_field_values)):
+            if i == len(source_field_values)-1:
+                source_str += source_field_values[i]
+            else:
+                source_str += source_field_values[i] + ","
+
+        for i in range(len(destination_field_values)):
+            if i == len(destination_field_values)-1:
+                dest_str += destination_field_values[i]
+            else:
+                dest_str += destination_field_values[i] + ","
+
+        rule = "CrossDatabase_Insert" \
+               "[[-SEP-]]SOURCE_FORM_ID" \
+               "[[-SEP-]]SOURCE_FORM" \
+               "[[-SEP-]]SOURCE_FORM_ID" \
+               "[[-SEP-]]SOURCE_FIELD_VALUES" \
+               "[[-SEP-]]" \
+               "[[-SEP-]]DEST_FORM_ID" \
+               "[[-SEP-]]DESTINATION_FIELD_VALUES" \
+               "[[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]]" \
+               "[[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]][[-SEP-]]"
+
+        rule = rule.replace("SOURCE_FORM_ID", source_id) \
+            .replace("SOURCE_FORM", source_form) \
+            .replace("SOURCE_FIELD_VALUES", source_str) \
+            .replace("DEST_FORM_ID", destination_id) \
+            .replace("DESTINATION_FIELD_VALUES", dest_str)
         return rule
